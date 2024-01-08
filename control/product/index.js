6c6ea2365b8f3885e3f4832353e7f676cc8f3041
Kuldipsinh Jadeja <jkuldipsinh067@gmail.com>
2024-01-08 13:03:59 +0530

    multer with cloudinary done

--- control/product/index.js
+++ control/product/index.js
@@ -1,25 +1,42 @@
 const model = require("../../schems/product/index");
 const productSchema = model.productSchema;
 const multer = require('multer')
+const fs = require('fs')
+let fileName = ''
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({ 
+  cloud_name: 'dk7xliskx',
+  api_key: '438692264925247',
+  api_secret: 'u4qojjVcPmV8Nf6_ShKW94tjSpw', 
+});
 const storage = multer.diskStorage({
-  destination: './',
+  destination: './upload',
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname); 
+    fileName = Date.now() + '-' + file.originalname
+    cb(null, fileName); 
   }
 });
-
 const upload = multer({ storage: storage });
-exports.addProducts = async (req, res) => {
-  console.log(req.body)
-  try {
-    // upload.single('image')
-    const newProduct = new productSchema(req.body[0]);
-    newProduct.save();
-    res.json(newProduct).status(200);
-  } catch (error) {
-    console.log(error);
-    res.json(error);
+const middleware = upload.single('image');
+
+exports.addProducts =  async (req, res, next) => {
+  let Add = async () => {
+    try{
+      const result = await cloudinary.uploader.upload(req.file.path);
+      if(result.asset_id){
+        fs.unlinkSync(req.file.path)
+        req.body.img = result.url
+        const newProduct = new productSchema(req.body);
+        newProduct.save();
+        res.json(newProduct).status(200);
+      }
+    }catch(e){
+      console.log(e);
+      res.json(e)
+    }
   }
+  middleware(req, res, Add);
+  
 };
 
 exports.getProducts = async (req, res) => {
