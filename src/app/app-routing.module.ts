b54a709f0f64daf0308581a801d296cffb683d14
Kuldipsinh Jadeja <jkuldipsinh067@gmail.com>
2024-01-10 17:47:47 +0530

    managing categories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './forms/signin/signin.component';
+import { isSignInGuard } from './is-sign-in.guard';
 
 const routes: Routes = [
   {path: "", pathMatch:"full", redirectTo: 'sign'},
   {path: "sign", component: SigninComponent},
-  {path: "dashboard", loadChildren: () => import('./pages/pages.module').then(mod => mod.PagesModule)}
+  {path: "dashboard", loadChildren: () => import('./pages/pages.module').then(mod => mod.PagesModule), canActivateChild:[isSignInGuard]}
 ];
 
 @NgModule({
