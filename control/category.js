72f12d0335b7f3c726b7473704446a8c8c58a6ad
Kuldipsinh Jadeja <jkuldipsinh067@gmail.com>
2024-01-12 12:15:55 +0530

    Adding category and Multi sub category is done.

--- control/category.js
+++ control/category.js
@@ -1,65 +1,73 @@
 const { CateSchema, subCateSchema } = require("../schemas/index");
 
-exports.findCategoy = async(req, res) => {
+exports.findCategoy = async (req, res) => {
   try {
-    const category = await CateSchema.findOne(req.params.id ? {_id: req.params.id } : {category: req.params.name})
-    res.json(category).status(200)
+    const category = await CateSchema.findOne(
+      req.params.id ? { _id: req.params.id } : { category: req.params.name }
+    );
+    res.json(category).status(200);
   } catch (error) {
-    res.json(error)
+    res.json(error);
   }
-}
+};
 
-exports.findSubCategoy = async(req, res) => {
+exports.findSubCategoy = async (req, res) => {
   try {
-    const sub_category = await subCateSchema.findOne(req.params.id ? {_id: req.params.id } : {subCategory: req.params.name})
-    res.json(sub_category).status(200)
+    const sub_category = await subCateSchema.findOne(
+      req.params.id ? { _id: req.params.id } : { subCategory: req.params.name }
+    );
+    res.json(sub_category).status(200);
   } catch (error) {
-    res.json(error)
+    res.json(error);
   }
-}
+};
 
 exports.AddCategory = async (req, res) => {
   try {
-    const newCategory = await CateSchema.findOne({category: req.body.Category})
-    const category = req.body.Category
-    if(!newCategory?._id){
-      newCategory = new CateSchema({ category: category.trim() });
+    const category = req.body.Category.trim();
+    let newCategory = await CateSchema.findOne({ category: category });
+    if (!newCategory?._id) {
+      newCategory = new CateSchema({ category: req.body.Category });
       newCategory.save();
     }
-    const SubCategories = []
-    req.body.SubCategory?.forEach(el => {
-      if(el){
+    const SubCategories = [];
+    console.log(newCategory);
+    req.body.SubCategory?.forEach((el) => {
+      if (el) {
         const newSubCategory = new subCateSchema({
           subCategory: el.trim(),
-          cateId: category.trim(),
-        });  
+          cateId: newCategory.category,
+        });
         newSubCategory.save();
-        SubCategories.push(newSubCategory)
+        SubCategories.push(newSubCategory);
       }
     });
-    res.json({
-      category: newCategory,
-      sub_categories: SubCategories
-    }).status(200);
+    res
+      .json({
+        category: newCategory,
+        sub_categories: SubCategories,
+      })
+      .status(200);
   } catch (error) {
+    console.log(error);
     res.json(error);
   }
 };
 
 exports.findCategories = async (req, res) => {
   try {
-    const categories = await CateSchema.find()
-    res.json(categories)
+    const categories = await CateSchema.find();
+    res.json(categories);
   } catch (error) {
-    res.json(error)
+    res.json(error);
   }
-}
+};
 
 exports.findSubCategories = async (req, res) => {
   try {
-    const sub_categories = await subCateSchema.find({cateId: req.params.id})
-    res.json(sub_categories)
+    const sub_categories = await subCateSchema.find({ cateId: req.params.id });
+    res.json(sub_categories);
   } catch (error) {
-    res.json(error)
+    res.json(error);
   }
-}
+};
