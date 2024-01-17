2b43c3eeaccbae0f82f30fd43295d088fdffcaa8
Kuldipsinh Jadeja <jkuldipsinh067@gmail.com>
2024-01-17 17:43:47 +0530

    Devlopment is complete of QuatumGoods

--- mddleware/index.js
+++ mddleware/index.js
@@ -2,9 +2,9 @@ const jwt = require("jsonwebtoken");
 
 exports.verify = async (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
-    if(!token) res.status(401).status({status: 401})
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (e) => {
-    if(!e) next()
-    else res.status(401).json({status: 401});
-    });
-}; 
+  if (!token) res.status(401).status({ status: 401 });
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (e) => {
+    if (!e) next();
+    else res.status(401).json({ status: 401 });
+  });
+};
